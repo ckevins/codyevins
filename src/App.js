b3a25485609212f27a0f41247271afc1f91aaabc
Cody Evins <cody.evins@gmail.com>
2021-03-09 20:44:59 -0500

    Home page built, react router implemented, resume started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+import { Home } from './home/home';
+import { Resume } from './resume/resume';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/resume'>Resume</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path='/resume'>
+              <Resume/>
+            </Route>
+            <Route path='/'>
+              <Home/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/home/home.js
@@ -0,0 +1,16 @@
+import logo from '../cody-evins-logo.png';
+
+
+export function Home() {
+    return (
+        <div>
+            <header className="App-header">
+                <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&display=swap" rel="stylesheet"/> 
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1>Cody Evins</h1>
+                <h3>Full Stack Web Developer</h3>
+            </header>
+        </div>
+    )
+}
